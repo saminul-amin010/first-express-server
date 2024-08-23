0deb09b18516c0c8fd37c5e376c8269f2bca2381
Md. Saminul Amin <saminul.amin@gmail.com>
2024-08-23 18:09:05 +0600

    about and contact and nodemon added

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Us</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Us</h1>");
+});
+
 app.listen(port, () => {
-    console.log(`Server running in Port ${port}.`);
-})
+  console.log(`Server running in Port ${port}.`);
+});
